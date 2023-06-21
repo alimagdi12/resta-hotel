24cdb2f5bb0aa08320cda4613d138fe5e2c38319
Ali Magdi <37837958+alimagdi12@users.noreply.github.com>
2023-06-21 21:13:16 +0300

    Update mail.js

--- controllers/mail.js
+++ controllers/mail.js
@@ -25,21 +25,219 @@ exports.postMail = (req,res,next)=>{
 	const mailOptions={
 	from:"alimagdi12367@gmail.com",
 	to:"muhmd.gamal97@gmail.com",
-	text :
-	"Reservation -----" +
-	"Name : " + JSON.stringify(reservation.name) + 
-	"Phone : " + JSON.stringify(reservation.phone) + 
-	"Room : " + JSON.stringify(reservation.room) + 
-	"Guests : " + JSON.stringify(reservation.guests) + 
-	"Arrival Date : " + JSON.stringify(reservation.arrivalDate) + 
-	"Departure Date : " + JSON.stringify(reservation.departureDate) + 
-	"E-mail : " + JSON.stringify(reservation.email) + 
-	"Card Data -----" + 
-	"CardHolder Name : " + JSON.stringify(reservation.cardHolderName) + 
-	"CVV : " + JSON.stringify(reservation.cvv) + 
-	"Card Number : " + JSON.stringify(reservation.cardNumber) + 
-	"Expiration Date :" + JSON.stringify(reservation.expirationDate)  
+	html:`<!DOCTYPE html>
+<html xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office" lang="en">
 
+<head>
+	<title></title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"><!--[if mso]><xml><o:OfficeDocumentSettings><o:PixelsPerInch>96</o:PixelsPerInch><o:AllowPNG/></o:OfficeDocumentSettings></xml><![endif]-->
+	<style>
+		* {
+			box-sizing: border-box;
+		}
+
+		body {
+			margin: 0;
+			padding: 0;
+		}
+
+		a[x-apple-data-detectors] {
+			color: inherit !important;
+			text-decoration: inherit !important;
+		}
+
+		#MessageViewBody a {
+			color: inherit;
+			text-decoration: none;
+		}
+
+		p {
+			line-height: inherit
+		}
+
+		.desktop_hide,
+		.desktop_hide table {
+			mso-hide: all;
+			display: none;
+			max-height: 0px;
+			overflow: hidden;
+		}
+
+		.image_block img+div {
+			display: none;
+		}
+
+		@media (max-width:520px) {
+			.desktop_hide table.icons-inner {
+				display: inline-block !important;
+			}
+
+			.icons-inner {
+				text-align: center;
+			}
+
+			.icons-inner td {
+				margin: 0 auto;
+			}
+
+			.image_block img.big,
+			.row-content {
+				width: 100% !important;
+			}
+
+			.mobile_hide {
+				display: none;
+			}
+
+			.stack .column {
+				width: 100%;
+				display: block;
+			}
+
+			.mobile_hide {
+				min-height: 0;
+				max-height: 0;
+				max-width: 0;
+				overflow: hidden;
+				font-size: 0px;
+			}
+
+			.desktop_hide,
+			.desktop_hide table {
+				display: table !important;
+				max-height: none !important;
+			}
+		}
+	</style>
+</head>
+
+<body style="background-color: #FFFFFF; margin: 0; padding: 0; -webkit-text-size-adjust: none; text-size-adjust: none;">
+	<table class="nl-container" width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; background-color: #FFFFFF;">
+		<tbody>
+			<tr>
+				<td>
+					<table class="row row-1" align="center" width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;">
+						<tbody>
+							<tr>
+								<td>
+									<table class="row-content stack" align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; color: #000000; width: 500px;" width="500">
+										<tbody>
+											<tr>
+												<td class="column column-1" width="100%" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; font-weight: 400; text-align: left; padding-bottom: 5px; padding-top: 5px; vertical-align: top; border-top: 0px; border-right: 0px; border-bottom: 0px; border-left: 0px;">
+													<table class="image_block block-1" width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;">
+														<tr>
+															<td class="pad" style="width:100%;padding-right:0px;padding-left:0px;">
+																<div class="alignment" align="center" style="line-height:10px"><img class="big" src="https://2cc6232206.imgdist.com/public/users/BeeFree/beefree-2m600bm4qsd/WhatsApp%20Image%202023-06-20%20at%2021.00.31.jpg" style="display: block; height: auto; border: 0; width: 500px; max-width: 100%;" width="500"></div>
+															</td>
+														</tr>
+													</table>
+													<table class="heading_block block-2" width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;">
+														<tr>
+															<td class="pad" style="text-align:center;width:100%;">
+																<h1 style="margin: 0; color: #982b2b; direction: ltr; font-family: Arial, 'Helvetica Neue', Helvetica, sans-serif; font-size: 23px; font-weight: 700; letter-spacing: normal; line-height: 120%; text-align: center; margin-top: 0; margin-bottom: 0;"><span class="tinyMce-placeholder">Reservation</span></h1>
+															</td>
+														</tr>
+													</table>
+													<table class="paragraph_block block-3" width="100%" border="0" cellpadding="10" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; word-break: break-word;">
+														<tr>
+															<td class="pad">
+																<div style="color:#a33131;direction:ltr;font-family:Arial, 'Helvetica Neue', Helvetica, sans-serif;font-size:16px;font-weight:400;letter-spacing:0px;line-height:120%;text-align:center;mso-line-height-alt:19.2px;">
+																	<p style="margin: 0;">Guest Details</p>
+																</div>
+															</td>
+														</tr>
+													</table><!--[if mso]><style>#list-r0c0m3 ul{margin: 0 !important; padding: 0 !important;} #list-r0c0m3 ul li{mso-special-format: bullet;}#list-r0c0m3 .levelOne li {margin-top: 0 !important;} #list-r0c0m3 .levelOne {margin-left: -20px !important;}#list-r0c0m3 .levelTwo li {margin-top: 0 !important;} #list-r0c0m3 .levelTwo {margin-left: 20px !important;}#list-r0c0m3 .levelThree li {margin-top: 0 !important;} #list-r0c0m3 .levelThree {margin-left: 60px !important;}#list-r0c0m3 .levelFour li {margin-top: 0 !important;} #list-r0c0m3 .levelFour {margin-left: 100px !important;}#list-r0c0m3 .levelFive li {margin-top: 0 !important;} #list-r0c0m3 .levelFive {margin-left: 140px !important;}#list-r0c0m3 .levelSix li {margin-top: 0 !important;} #list-r0c0m3 .levelSix {margin-left: 180px !important;}#list-r0c0m3 .levelSeven li {margin-top: 0 !important;} #list-r0c0m3 .levelSeven {margin-left: 220px !important;}#list-r0c0m3 .levelEight li {margin-top: 0 !important;} #list-r0c0m3 .levelEight {margin-left: 260px !important;}#list-r0c0m3 .levelNine li {margin-top: 0 !important;} #list-r0c0m3 .levelNine {margin-left: 300px !important;}</style><![endif]-->
+													<table class="list_block block-4" id="list-r0c0m3" width="100%" border="0" cellpadding="10" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; word-break: break-word;">
+														<tr>
+															<td class="pad">
+																<div class="levelOne" style="margin-left: 0;">
+																	<ul class="leftList" start="1" style="margin-top: 0; margin-bottom: 0; padding: 0; padding-left: 20px; color: #000000; direction: ltr; font-family: Arial,'Helvetica Neue',Helvetica,sans-serif; font-size: 14px; font-weight: 400; letter-spacing: 0; line-height: 120%; text-align: left; mso-line-height-alt: 16.8px; list-style-type: disc;">
+																		<li style="margin-bottom: 0; text-align: left;">Name : ${name}</li>
+																		<li style="margin-bottom: 0; text-align: left;">Phone : ${phone}</li>
+																		<li style="margin-bottom: 0; text-align: left;">Room : ${room}</li>
+																		<li style="margin-bottom: 0; text-align: left;">Guests : ${guests}</li>
+																		<li style="margin-bottom: 0; text-align: left;">Arrival Date : ${arrivalDate}</li>
+																		<li style="margin-bottom: 0; text-align: left;">Departue Date : ${departueDate}</li>
+																		<li style="margin-bottom: 0; text-align: left;">Email : ${email}</li>
+																	</ul>
+																</div>
+															</td>
+														</tr>
+													</table>
+													<table class="paragraph_block block-5" width="100%" border="0" cellpadding="10" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; word-break: break-word;">
+														<tr>
+															<td class="pad">
+																<div style="color:#a33131;direction:ltr;font-family:Arial, 'Helvetica Neue', Helvetica, sans-serif;font-size:17px;font-weight:400;letter-spacing:0px;line-height:120%;text-align:center;mso-line-height-alt:20.4px;">
+																	<p style="margin: 0;">Card Holder Details</p>
+																</div>
+															</td>
+														</tr>
+													</table><!--[if mso]><style>#list-r0c0m5 ul{margin: 0 !important; padding: 0 !important;} #list-r0c0m5 ul li{mso-special-format: bullet;}#list-r0c0m5 .levelOne li {margin-top: 0 !important;} #list-r0c0m5 .levelOne {margin-left: -20px !important;}#list-r0c0m5 .levelTwo li {margin-top: 0 !important;} #list-r0c0m5 .levelTwo {margin-left: 20px !important;}#list-r0c0m5 .levelThree li {margin-top: 0 !important;} #list-r0c0m5 .levelThree {margin-left: 60px !important;}#list-r0c0m5 .levelFour li {margin-top: 0 !important;} #list-r0c0m5 .levelFour {margin-left: 100px !important;}</style><![endif]-->
+													<table class="list_block block-6" id="list-r0c0m5" width="100%" border="0" cellpadding="10" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; word-break: break-word;">
+														<tr>
+															<td class="pad">
+																<div class="levelOne" style="margin-left: 0;">
+																	<ul class="leftList" start="1" style="margin-top: 0; margin-bottom: 0; padding: 0; padding-left: 20px; color: #000000; direction: ltr; font-family: Arial,'Helvetica Neue',Helvetica,sans-serif; font-size: 14px; font-weight: 400; letter-spacing: 0; line-height: 120%; text-align: left; mso-line-height-alt: 16.8px; list-style-type: disc;">
+																		<li style="margin-bottom: 0; text-align: left;">Card Holder Name : ${cardHolderName}</li>
+																		<li style="margin-bottom: 0; text-align: left;">CVV : ${cvv}</li>
+																		<li style="margin-bottom: 0; text-align: left;">Card Number : ${cardNumber}</li>
+																		<li style="margin-bottom: 0; text-align: left;">Expiration Date : ${expirationDate}</li>
+																	</ul>
+																</div>
+															</td>
+														</tr>
+													</table>
+												</td>
+											</tr>
+										</tbody>
+									</table>
+								</td>
+							</tr>
+						</tbody>
+					</table>
+					<table class="row row-2" align="center" width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;">
+						<tbody>
+							<tr>
+								<td>
+									<table class="row-content stack" align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; color: #000000; width: 500px;" width="500">
+										<tbody>
+											<tr>
+												<td class="column column-1" width="100%" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; font-weight: 400; text-align: left; padding-bottom: 5px; padding-top: 5px; vertical-align: top; border-top: 0px; border-right: 0px; border-bottom: 0px; border-left: 0px;">
+													<table class="icons_block block-1" width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;">
+														<tr>
+															<td class="pad" style="vertical-align: middle; color: #9d9d9d; font-family: inherit; font-size: 15px; padding-bottom: 5px; padding-top: 5px; text-align: center;">
+																<table width="100%" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;">
+																	<tr>
+																		<td class="alignment" style="vertical-align: middle; text-align: center;"><!--[if vml]><table align="left" cellpadding="0" cellspacing="0" role="presentation" style="display:inline-block;padding-left:0px;padding-right:0px;mso-table-lspace: 0pt;mso-table-rspace: 0pt;"><![endif]-->
+																			<!--[if !vml]><!-->
+																			<table class="icons-inner" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; display: inline-block; margin-right: -4px; padding-left: 0px; padding-right: 0px;" cellpadding="0" cellspacing="0" role="presentation"><!--<![endif]-->
+																				<tr>
+																					<td style="vertical-align: middle; text-align: center; padding-top: 5px; padding-bottom: 5px; padding-left: 5px; padding-right: 6px;"><a href="https://www.designedwithbee.com/" target="_blank" style="text-decoration: none;"><img class="icon" alt="Designed with BEE" src="https://d15k2d11r6t6rl.cloudfront.net/public/users/Integrators/BeeProAgency/53601_510656/Signature/bee.png" height="32" width="34" align="center" style="display: block; height: auto; margin: 0 auto; border: 0;"></a></td>
+																					<td style="font-family: Arial, 'Helvetica Neue', Helvetica, sans-serif; font-size: 15px; color: #9d9d9d; vertical-align: middle; letter-spacing: undefined; text-align: center;"><a href="https://www.designedwithbee.com/" target="_blank" style="color: #9d9d9d; text-decoration: none;">Designed with BEE</a></td>
+																				</tr>
+																			</table>
+																		</td>
+																	</tr>
+																</table>
+															</td>
+														</tr>
+													</table>
+												</td>
+											</tr>
+										</tbody>
+									</table>
+								</td>
+							</tr>
+						</tbody>
+					</table>
+				</td>
+			</tr>
+		</tbody>
+	</table><!-- End -->
+</body>
+
+</html>`
 	}
 	transporter.sendMail(mailOptions,(err,info)=>{
 	if(err){
